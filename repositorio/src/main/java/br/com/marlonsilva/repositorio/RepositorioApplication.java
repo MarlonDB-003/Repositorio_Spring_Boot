ff27f1332499aae3d6da0e33e650dca81df90038
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-26 15:24:58 -0400

    criação do projeto

---
+++ repositorio/src/main/java/br/com/marlonsilva/repositorio/RepositorioApplication.java
@@ -0,0 +1,13 @@
+package br.com.marlonsilva.repositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioApplication.class, args);
+	}
+
+}
---
+++ repositorio/src/test/java/br/com/marlonsilva/repositorio/RepositorioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marlonsilva.repositorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
