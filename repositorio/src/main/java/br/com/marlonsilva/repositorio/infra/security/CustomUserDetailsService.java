c8b74517bfb96ec7ba8fc3d1d1a39a80a65e7ae3
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-29 11:32:59 -0400

    Update CustomUserDetailsService

--- repositorio/src/main/java/br/com/marlonsilva/repositorio/infra/security/CustomUserDetailsService.java
+++ repositorio/src/main/java/br/com/marlonsilva/repositorio/infra/security/CustomUserDetailsService.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import br.com.marlonsilva.repositorio.domain.user.User;
 import br.com.marlonsilva.repositorio.repositories.UserRepository;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     @Autowired
