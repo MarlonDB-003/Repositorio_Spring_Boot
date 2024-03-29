5aa2540d68a2d7850a658fd66cb365f6e71350de
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-29 10:47:47 -0400

    Update UserRepository

--- repositorio/src/main/java/br/com/marlonsilva/repositorio/repositories/UserRepository.java
+++ repositorio/src/main/java/br/com/marlonsilva/repositorio/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.marlonsilva.repositorio.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.marlonsilva.repositorio.domain.user.User;
 
 public interface UserRepository extends JpaRepository<User, String>{
-    
+    Optional<User> findByEmail(String email);
 }
