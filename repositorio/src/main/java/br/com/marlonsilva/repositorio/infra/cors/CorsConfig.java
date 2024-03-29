c1c2c7cf2ad1a9d6cfff600f121e1afbb5ee0b41
MarlonDB-003 <marlondeividmd@gmail.com>
2024-03-29 11:37:20 -0400

    Criação classe CorsConfig

---
+++ repositorio/src/main/java/br/com/marlonsilva/repositorio/infra/cors/CorsConfig.java
@@ -0,0 +1,17 @@
+package br.com.marlonsilva.repositorio.infra.cors;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")//porta onde roda o frontend
+                .allowedMethods("GET", "POST");
+    }
+    
+}
